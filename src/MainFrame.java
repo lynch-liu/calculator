a5d18eba47b4177eaa097557d71ce53452e2ee35
lynch <lynch.liu@hotmail.com>
2018-05-22 22:39:24 -0700

    basic math function finished

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,9 +1,17 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
 
 public class MainFrame {
+    private final int PLUS = 1;
+    private final int MINUS = 2;
+    private final int MULTIPLY = 3;
+    private final int DIVID = 4;
+    private final int SQ = 5;
+    private final int CB = 6;
+    private final int SQRT = 7;
+    private final int RM = 8;
+    private final int NOOP = 0;
     private JPanel panel1;
     private JButton oneButton;
     private JButton xAssignButton;
@@ -12,7 +20,6 @@ public class MainFrame {
     private JButton zButton;
     private JButton sqrtButton;
     private JButton historyButton;
-    private JLabel mainLabel;
     private JButton CBotton;
     private JButton equalButton;
     private JButton twoButton;
@@ -38,8 +45,7 @@ public class MainFrame {
     private JLabel xLabel;
     private JLabel yLabel;
     private JLabel zLabel;
-    private JLabel subLabel;
-
+    private JTextField mainLabel;
     private double result = 0;
     private double prev = 0;
     private boolean havePrev = false;
@@ -50,20 +56,19 @@ public class MainFrame {
     private double z = 0;
     private boolean haveZ = false;
     private int operation = 0;
-    private boolean
+    private boolean haveDot = false;
 
-    private final int PLUS = 1;
-    private final int MINUS = 2;
-    private final int MULTIPLY = 3;
-    private final int DIVID = 4;
-    private final int SQ= 5;
-    private final int CB= 6;
-    private final int SQRT= 7;
-    private final int RM= 7;
-    private final int NOOP= 0;
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainFrame");
+        frame.setContentPane(new MainFrame().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
     public MainFrame() {
+        mainLabel.disable();
         historyButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -71,122 +76,190 @@ public class MainFrame {
             }
         });
 
-
+        /////////////////////////////////////////////////////
         // assign and recall function for X Y Z
+        /////////////////////////////////////////////////////
         xAssignButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
-                x = result;
-                xLabel.setText(""+x);
+                if(mainLabel.getText().substring(mainLabel.getText().length()-2 , mainLabel.getText().length()-1) == ".")
+                    mainLabel.setText(mainLabel.getText()+"0");
+
+                x = Double.parseDouble(mainLabel.getText());
+                xLabel.setText("" + x);
+                haveX = true;
             }
         });
         yAssignButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
-                y = result;
-                yLabel.setText(""+y);
+
+                y = Double.parseDouble(mainLabel.getText());
+                yLabel.setText("" + y);
+                haveY = true;
             }
         });
         zAssignButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                z = Double.parseDouble(mainLabel.getText());
+                zLabel.setText("" + z);
+                haveZ = true;
             }
         });
         xButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if (haveX) {
+                    mainLabel.setText(x + "");
+                }
             }
         });
         yButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if (haveY) {
+                    mainLabel.setText(y + "");
+                }
             }
         });
         zButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
-                z = result;
-                zLabel.setText(""+z);
+                if (haveZ) {
+                    mainLabel.setText(z + "");
+                }
             }
         });
         /////////////////////////////////////////////////////
         //Function buttons
-
-        CBotton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                clearButtonPushed();
-            }
-        });
+        /////////////////////////////////////////////////////
         plusButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                if(mainLabel.getText().length() == 0){
+                }else if(havePrev){
+                    updateResult();
+                    prev = result;
+                    clearLabel();
+                }else {
+                    prev = Double.parseDouble(mainLabel.getText());
+                    havePrev = true;
+                    clearLabel();
+                }
                 operation = PLUS;
-                
+
             }
         });
         minusButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                if(mainLabel.getText().length() == 0){
+                }else if(havePrev){
+                    updateResult();
+                    prev = result;
+                    clearLabel();
+                }else {
+                    prev = Double.parseDouble(mainLabel.getText());
+                    havePrev = true;
+                    clearLabel();
+                }
                 operation = MINUS;
             }
         });
         multiplyButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                if(mainLabel.getText().length() == 0){
+                }else if(havePrev){
+                    updateResult();
+                    prev = result;
+                    clearLabel();
+                }else {
+                    prev = Double.parseDouble(mainLabel.getText());
+                    havePrev = true;
+                    clearLabel();
+                }
                 operation = MULTIPLY;
             }
         });
         dividButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                if(mainLabel.getText().length() == 0){
+                }else if(havePrev){
+                    updateResult();
+                    prev = result;
+                    clearLabel();
+                }else {
+                    prev = Double.parseDouble(mainLabel.getText());
+                    havePrev = true;
+                    clearLabel();
+                }
                 operation = DIVID;
             }
         });
         remainderButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                if(mainLabel.getText().length() == 0){
+                }else if(havePrev){
+                    updateResult();
+                    prev = result;
+                    clearLabel();
+                }else {
+                    prev = Double.parseDouble(mainLabel.getText());
+                    havePrev = true;
+                    clearLabel();
+                }
                 operation = RM;
             }
         });
         squreButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                if(havePrev){
+                    updateResult();
+                    mainLabel.setText(""+result);
+                }
                 operation = SQ;
+                equalButtonPushed();
+
             }
         });
         cubeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                if(havePrev){
+                    updateResult();
+                    mainLabel.setText(""+result);
+                }
                 operation = CB;
+                equalButtonPushed();
             }
         });
         sqrtButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                if(havePrev){
+                    updateResult();
+                    mainLabel.setText(""+result);
+                }
                 operation = SQRT;
+                equalButtonPushed();
             }
         });
+        ////////////////////
         delButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(mainLabel.getText().length() > 0){
+                if (mainLabel.getText().length() > 0) {
+
                     String tempS = mainLabel.getText();
-                    tempS = tempS.substring(0,tempS.length()-2);
+                    if(mainLabel.getText().charAt(mainLabel.getText().length()-1) == '.')
+                        haveDot = false;
+                    tempS = tempS.substring(0, tempS.length() - 1);
                     mainLabel.setText(tempS);
                 }
             }
@@ -194,10 +267,17 @@ public class MainFrame {
         equalButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                updateResult();
+                equalButtonPushed();
+                prev = result;
+
             }
         });
-
+        CBotton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearButtonPushed();
+            }
+        });
         //////////////////////////////////////////////////////////
         //Digit Button will add a char to the mainLabel
         oneButton.addActionListener(new ActionListener() {
@@ -263,54 +343,100 @@ public class MainFrame {
         dotButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                addTextToField(".");
+                if (!haveDot) {
+                    addTextToField(".");
+                    haveDot = true;
+                }
             }
         });
 
 
     }
+
     //update result value
-    private void updateResult(){
-        switch(result){
+    private void updateResult() {
+        if (mainLabel.getText().charAt(mainLabel.getText().length() - 1) == '.'){
+            mainLabel.setText(mainLabel.getText() + "0");
+        }
+        if(mainLabel.getText().length() == 0){
+            mainLabel.setText("0.0");
+        }
+        switch (operation) {
             case PLUS:
-                result = prev + Double.parseDouble(mainLabel.getText());
+                if (havePrev) {
+                    result = prev + Double.parseDouble(mainLabel.getText());
+                }
                 break;
             case MINUS:
-                result = prev - Double.parseDouble(mainLabel.getText());
+                if (havePrev) {
+                    result = prev - Double.parseDouble(mainLabel.getText());
+                }
                 break;
             case MULTIPLY:
-                result = prev * Double.parseDouble(mainLabel.getText());
+                if (havePrev) {
+                    result = prev * Double.parseDouble(mainLabel.getText());
+                }
                 break;
             case DIVID:
-                result = prev / Double.parseDouble(mainLabel.getText());
+                if (havePrev) {
+                    result = prev / Double.parseDouble(mainLabel.getText());
+                }
+                break;
+            case RM:
+                if (havePrev) {
+                    result = prev % Double.parseDouble(mainLabel.getText());
+                }
                 break;
             case SQ:
-                result = (Double.parseDouble(mainLabel.getText()))*(Double.parseDouble(mainLabel.getText()));
+                result = (Double.parseDouble(mainLabel.getText())) * (Double.parseDouble(mainLabel.getText()));
                 break;
             case CB:
-                result = (Double.parseDouble(mainLabel.getText()))*(Double.parseDouble(mainLabel.getText()))*(Double.parseDouble(mainLabel.getText()));
+                result = (Double.parseDouble(mainLabel.getText())) * (Double.parseDouble(mainLabel.getText())) * (Double.parseDouble(mainLabel.getText()));
                 break;
             case SQRT:
                 result = Math.sqrt(Double.parseDouble(mainLabel.getText()));
                 break;
+            default:
+                break;
         }
     }
-    //add a String to the mainLabel
-    private void addTextToField(String s){
-        if(mainLabel.getText().length()<8) {
-            mainLabel.setText(mainLabel.getText() + s);
-        }else{
 
+    private void showResult() {
+        mainLabel.setText(result + "");
+    }
+
+    //add a String to the mainLabel
+    private void addTextToField(String s) {
+        if (haveDot) {
+            mainLabel.setText(mainLabel.getText() + s);
+        } else {
+            if (mainLabel.getText().length() <= 8) {
+                mainLabel.setText(mainLabel.getText() + s);
+            }
         }
     }
-    private void addTextToSubField(String s){
-            subLabel.setText(subLabel.getText() + s);
+    //equal action
+    private void equalButtonPushed(){
+        if(havePrev){
+            updateResult();
+            operation = NOOP;
+            showResult();
+        }else{
+            updateResult();
+            operation = NOOP;
+            showResult();
+        }
     }
-    //clear the mainLabel
-    private void clearButtonPushed(){
-        mainLabel.setText("");
+    //clearbutton's action
+    private void clearButtonPushed() {
+        clearLabel();
         havePrev = false;
         operation = NOOP;
+    }
+
+    private void clearLabel() {
+        mainLabel.setText("");
+        haveDot = false;
     }
 
 }
